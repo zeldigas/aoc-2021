8bdfe8f5b6f974596de93db67ceeb94405deb09d
Dmitry Pavlov <dpavlov@aligntech.com>
2021-12-02 00:18:59 +0300

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,20 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.asSequence().filter { it.isNotBlank() }.map { it.toInt() }
+            .windowed(2).count { it[0] < it[1] }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.asSequence().filter { it.isNotBlank() }.map { it.toInt() }
+            .windowed(3).map { it[0] + it[1] + it[2] }
+            .windowed(2).count { it[0] < it[1] }
+
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
